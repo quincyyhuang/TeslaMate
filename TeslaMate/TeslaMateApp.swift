d24cb88d55336dbdcabf74ffbf68266e8ef264b1
Quincy Huang <quincyyhuang@gmail.com>
2026-02-17 20:11:57 -0500

    Initial Commit

---
+++ TeslaMate/TeslaMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaMateApp.swift
+//  TeslaMate
+//
+//  Created by Quincy Huang on 2/17/26.
+//
+
+import SwiftUI
+
+@main
+struct TeslaMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
